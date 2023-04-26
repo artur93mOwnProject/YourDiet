1149227c70bcdda388b5186718f40561205d0db3
artur93mOwnProject <artur93mm@gmail.com>
2023-04-26 20:30:16 +0200

    Added bootstrap and initial Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<Header></Header>
+		</div>
+	);
 }
 
 export default App;
