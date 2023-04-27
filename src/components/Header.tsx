ccbf05b2445921e24e9c77383b51571a718e8323
artur93mOwnProject <artur93mm@gmail.com>
2023-04-27 21:33:10 +0200

    Added menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,27 +26,34 @@ const NavigationBar = () => {
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav" className="px-3">
-				<Nav>
-					<Nav.Link href="#home">{t('home.page')}</Nav.Link>
-					<Nav.Link href="#link">Link</Nav.Link>
-
-					<NavDropdown
-						title={<Globe className="fs-5" />}
-						id="basic-nav-dropdown"
-					>
-						<NavDropdown.Item
-							onClick={onChangeLanguage('pl')}
-							active={isActive('pl')}
+				<Nav className="d-flex justify-content-between w-100">
+					<div className="d-flex flex-column flex-lg-row">
+						<Nav.Link href="#home">{t('domain.home.page')}</Nav.Link>
+						<Nav.Link href="#yourdiet">{t('domain.your.diet')}</Nav.Link>
+						<Nav.Link href="#insertmeal">{t('domain.add.meal')}</Nav.Link>
+						<Nav.Link href="#browsemeal">{t('domain.browse.meal')}</Nav.Link>
+					</div>
+					<div className="d-flex flex-column flex-lg-row">
+						<NavDropdown
+							className="ml-5"
+							title={<Globe className="fs-5" />}
+							id="basic-nav-dropdown"
 						>
-							{t('language.polish')}
-						</NavDropdown.Item>
-						<NavDropdown.Item
-							onClick={onChangeLanguage('en')}
-							active={isActive('en')}
-						>
-							{t('language.english')}
-						</NavDropdown.Item>
-					</NavDropdown>
+							<NavDropdown.Item
+								onClick={onChangeLanguage('pl')}
+								active={isActive('pl')}
+							>
+								{t('language.polish')}
+							</NavDropdown.Item>
+							<NavDropdown.Item
+								onClick={onChangeLanguage('en')}
+								active={isActive('en')}
+							>
+								{t('language.english')}
+							</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Link href="#login">{t('domain.login')}</Nav.Link>
+					</div>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
